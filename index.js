7f49aab32a1a8001569b7e8110cc30e05bbabd5b
estuardo9609 <estuardoestrada_54@hotmail.com>
2018-10-09 20:26:47 -0600

    Metodo Post y Put funcionales

--- index.js
+++ index.js
@@ -3,17 +3,43 @@ const app = express();
 var controller = require("./model/playlists.js");
 myPlaylists= controller.playlists;
 
+app.use(express.json());
+
 app.get('/api/playlists', (req, res) => {
-    res.send(myPlaylists);
+    res.json(myPlaylists);
 });
 
-app.get('/api/playlist/:id', (req, res) => {
+app.get('/api/playlists/:id', (req, res) => {
     let playlist = myPlaylists.find(c => c.id == parseInt(req.params.id));
     if(!playlist) res.status(404).send('El playlist que busca no se encuentra'); //404
     res.status(200).json(playlist);
 });
 
+app.post('/api/playlists', (req, res) => {
+    const playlist = {
+        id: myPlaylists.length + 1,
+        name: req.body.name,
+        description: req.body.description,
+        genre: req.body.genre,
+        rate: req.body.rate,
+        hide: true
+    };
+    myPlaylists.push(playlist);
+    res.status(201).send(playlist);
+});
 
+app.put('/api/playlists/:id', (req, res) => {
+    const playlist = myPlaylists.find(c => c.id == parseInt(req.params.id));
+    if(!playlist) res.status(404).send('El playlist que busca no se encuentra'); //404
+    
+    playlist.name = req.body.name;
+    playlist.description = req.body.description;
+    playlist.genre = req.body.genre;
+    playlist.rate = req.body.rate;
+    playlist.hide = true;
+
+    res.status(204).send(playlist);
+});
 
 //PORT
 const port = process.env.PORT || 3001;
--- model/playlists.js
+++ model/playlists.js
@@ -1,9 +1,2 @@
 
-exports.playlists = [
-    { id: 1, name: 'Radio Music',
-        description: 'Best pop songs for 2018',
-        genre: 'Pop', rate: 5, hide: true},
-    { id: 2, name: 'Radio Music2',
-        description: 'Best pop songs for 2018',
-        genre: 'Pop', rate: 5, hide: true} 
-];
+exports.playlists = [];
